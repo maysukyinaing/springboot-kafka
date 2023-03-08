87ad1cab1d2932acb90730afaf4bea02a5f69f97
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-03-08 14:08:36 +0630

    producer

---
+++ kafka-producer-wikimedia/src/main/java/org/ms/configuration/KafkaTopicConfiguration.java
@@ -0,0 +1,17 @@
+package org.ms.configuration;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.internals.Topic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfiguration {
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("wikimedia_recentchange")
+                .build();
+    }
+}
