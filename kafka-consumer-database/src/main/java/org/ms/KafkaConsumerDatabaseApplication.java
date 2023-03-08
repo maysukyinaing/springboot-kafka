b304224c1b6ef1202609b577c0863361e9042daa
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-03-08 14:08:54 +0630

    consumer

---
+++ kafka-consumer-database/src/main/java/org/ms/KafkaConsumerDatabaseApplication.java
@@ -0,0 +1,12 @@
+package org.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerDatabaseApplication.class);
+    }
+}
---
+++ kafka-consumer-database/src/main/java/org/ms/repository/WikimediaDataRepository.java
@@ -0,0 +1,9 @@
+package org.ms.repository;
+
+import org.ms.model.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
